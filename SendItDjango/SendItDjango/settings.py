13eec04425cf9692ae3ae81700352f6e3125781a
Andrew Zakhary <zakhary0000@gmail.com>
2023-07-26 10:07:14 -0400

    Strung up framework for senditapi

--- SendItDjango/SendItDjango/settings.py
+++ SendItDjango/SendItDjango/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "SendItDjango"
+    "SendItDjango",
+    "rest_framework",
+    "sendit_api"
 ]
 
 MIDDLEWARE = [
---
+++ SendItDjango/sendit_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SenditApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sendit_api'
